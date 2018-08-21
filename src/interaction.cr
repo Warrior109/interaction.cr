1d32d159efa40d1f13d8b2ef7f825cda9bc1fd18
Olexandr Hoshylyk <gashuk95@gmail.com>
2018-08-21 04:43:37 +0000

    Init app

---
+++ src/interaction.cr
@@ -0,0 +1,6 @@
+require "./interaction/*"
+
+# TODO: Write documentation for `Interaction`
+module Interaction
+  # TODO: Put your code here
+end
---
+++ src/interaction/version.cr
@@ -0,0 +1,3 @@
+module Interaction
+  VERSION = "0.1.0"
+end
